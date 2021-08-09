b7d4d30a4dc804f29accb7dd1ab8e8aae56bd8c2
w8krl <44670608+w8krl@users.noreply.github.com>
2021-08-09 22:18:19 +0100

    Changes to Form

--- app/containers/Modals/NewResModal.js
+++ app/containers/Modals/NewResModal.js
@@ -9,53 +9,84 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import NewRes from '../ResManagement/Forms/NewRes';
 import MultiStepFrm from '../ResManagement/Forms/AddResMultiStep';
 import { makeStyles } from '@material-ui/core/styles';
-
-// Test
+import Axios from 'axios';
 
 const useStyles = makeStyles((theme) => ({
     large: {
-      minWidth: "70%"
+        minWidth: "70%"
     },
-    paperScrollPaper : {
-        minHeight:"80vh",
-        minWidth:"100vh"
+    paperScrollPaper: {
+        minHeight: "70vh",
+        minWidth: "60%"
+    },
+    closeButton: {
+        position: "absolute",
+        right: "1rem",
+        top: "1rem"
     }
-  }));
+}));
 
 export default function FormDialog() {
-  const [open, setOpen] = React.useState(false);
-  const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+    const classes = useStyles();
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+    const handleClose = () => {
+        setOpen(false);
+    };
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const form_data = new FormData();
+        for (var key in formData) {
+            form_data.append(key, formData[key]);
+        }
+        form_data.append("file", selectedFile);
 
+        Axios.post('http://localhost:8888/newRes', form_data)
+            .then(function (response) {
+                console.log(response.data);
+            })
 
-  return (
-      <div>
-          <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-              + Resource
-          </Button>
-          <Dialog classes={{paper: classes.paperScrollPaper}} open={open} maxWidth="sm" onClose={handleClose} aria-labelledby="form-dialog-title">
-              <DialogTitle id="form-dialog-title">Add New Resource 🧙‍♂️</DialogTitle>
-              <DialogContent>
-                  <DialogContentText>
-                      Resource will be added to the database. CVs will be automatically indexed and analysed for core skills.
-                  </DialogContentText>
-                  {/* <NewRes></NewRes> */}
-                  <MultiStepFrm></MultiStepFrm>
-              </DialogContent>
-              <DialogActions>
-                  <Button onClick={handleClose} color="primary">
-                    Close X
-                  </Button>
-              </DialogActions>
-          </Dialog>
-      </div>
-  );
+    };
+
+    const [formData, updateFormData] = React.useState();
+    const [selectedFile, setSelectedFile] = React.useState();
+
+    const handleChange = (e) => {
+
+        console.log(formData);
+        updateFormData({
+            ...formData,
+
+            // Trimming any whitespace
+            [e.target.name]: e.target.value.trim()
+        });
+    };
+    
+    return (
+        <div>
+            <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+                + Resource
+            </Button>
+            <Dialog classes={{ paper: classes.paperScrollPaper }} open={open} maxWidth="sm" onClose={handleClose} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">Add New Resource 🧙‍♂️
+
+                    <Button className={classes.closeButton} onClick={handleClose} color="primary">
+                        Close X
+                    </Button>
+                </DialogTitle>
+                <DialogContent>
+                    {/* <NewRes></NewRes> */}
+                    <MultiStepFrm handleSubmit={handleSubmit}></MultiStepFrm>
+                </DialogContent>
+                <DialogActions>
+                    {/* Resource will be added to the database. CVs will be automatically indexed and analysed for core skills. */}
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
 }
--- app/containers/ResManagement/Forms/AddResMultiStep.js
+++ app/containers/ResManagement/Forms/AddResMultiStep.js
@@ -2,42 +2,57 @@ import React, { Component } from 'react'
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
-import FileUpload from '@material-ui/icons/CloudUpload';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+
+
+
+const theme = createTheme({
+    overrides: {
+        // Style sheet name ⚛️
+        MuiFormControl: {
+            // Name of the rule
+            root: {
+                // Some CSS
+                width: '40%',
+                paddingRight: '2rem',
+                paddingBottom: "1rem"
+            },
+        },
+    },
+});
 
 const styles = theme => ({
-    demo: {
-      height: 'auto',
-    },
-    divider: {
-      display: 'block',
-      margin: `${theme.spacing(3)}px 0`,
-    },
-    field: {
-      margin: `${theme.spacing(3)}px 5px`,
-    },
     button: {
-      margin: theme.spacing(1),
+        margin: theme.spacing(1),
     },
-    inputUpload: {
-      display: 'none',
+    leftButton: {
+        position: "absolute",
+        bottom: "2rem",
+        left: "2rem"
     },
-    leftIcon: {
-      marginRight: theme.spacing(1),
-    },
-    rightIcon: {
-      marginLeft: theme.spacing(1),
-    },
-    iconSmall: {
-      fontSize: 20,
-    },
-  });
+    rightButton: {
+        position: "absolute",
+        bottom: "2rem",
+        right: "2rem"
+    }, MuiTextField: {
+        marginBottom: 20,
+        width: "50%",
+        paddingRight: "2rem"
+    }, field: {
+        padding: theme.spacing(1),
+        width: "50%"
+    }
+});
+
 class MasterForm extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
             currentStep: 1,
             file: '',
-            formData: { 
+            fileName: '',
+            formData: {
                 name: '',
                 email: '',
                 title: '',
@@ -51,172 +66,172 @@ class MasterForm extends React.Component {
                 co_type: '',
                 username: '',
             },
-            
+
         }
     }
 
     handleChange = event => {
         const { name, value } = event.target
         this.setState(prevState => ({
-            formData: {     
-              ...prevState.formData,
-              [name]: value
+            formData: {
+                ...prevState.formData,
+                [name]: value
             }
         }))
     }
-   
+
     fileChange = event => {
         const file = event.target.files[0];
-        this.setState({['file']:file})
+        this.setState({ ['file']: file, ['fileName']: file.name })
     };
 
-    handleSubmit = event => {
-        event.preventDefault()
-        console.log(this.state);
+    _next = () => {
+        let currentStep = this.state.currentStep
+        currentStep = currentStep >= 2 ? 3 : currentStep + 1
+        this.setState({
+            currentStep: currentStep
+        })
     }
 
-  _next = () => {
-    let currentStep = this.state.currentStep
-    currentStep = currentStep >= 2? 3: currentStep + 1
-    this.setState({
-      currentStep: currentStep
-    })
-  }
-    
-  _prev = () => {
-    let currentStep = this.state.currentStep
-    currentStep = currentStep <= 1? 1: currentStep - 1
-    this.setState({
-      currentStep: currentStep
-    })
-  }
+    _prev = () => {
+        let currentStep = this.state.currentStep
+        currentStep = currentStep <= 1 ? 1 : currentStep - 1
+        this.setState({
+            currentStep: currentStep
+        })
+    }
 
 
-previousButton() {
-  let currentStep = this.state.currentStep;
-  if(currentStep !==1){
-    return (
-      <button 
-        className="btn btn-secondary" 
-        type="button" onClick={this._prev}>
-      Previous
-      </button>
-    )
-  }
-  return null;
-}
+    previousButton(props) {
+        let currentStep = this.state.currentStep;
+        if (currentStep !== 1) {
+            return (
+                <Button
+                    className={props.leftButton} color="secondary" variant="outlined"
+                    type="button" onClick={this._prev}>
+                    Previous
+                </Button>
+            )
+        }
+        return null;
+    }
 
 
 
-nextButton(){
-  let currentStep = this.state.currentStep;
-  if(currentStep <3){
-    return (
-      <button 
-        className="btn btn-primary float-right" 
-        type="button" onClick={this._next}>
-      Next
-      </button>        
-    )
-  }
-  return null;
-}
-  
-  render() { 
-    return (
-      <React.Fragment>
-      <h1>New Resource Wizard</h1>
-      <p>Step {this.state.currentStep} </p> 
-      <form onSubmit={this.handleSubmit}>
-      {/* 
-        render the form steps and pass required props in
-      */}
-        <Step1 
-          currentStep={this.state.currentStep} 
-          handleChange={this.handleChange}
-          name={this.state.formData.name}
-          email={this.state.formData.email}
-          title={this.state.formData.title}
-          pri_contact_no={this.state.formData.pri_contact_no}
-          sec_contact_no={this.state.formData.sec_contact_no}
-          country={this.state.formData.country}
-          region={this.state.formData.region}
-        />
-        <Step2 
-          currentStep={this.state.currentStep} 
-          handleChange={this.handleChange}
-          comments={this.state.formData.comments}
-          contract_type={this.state.formData.contract_type}
-          management_co={this.state.formData.management_co}
-        />
-        <Step3 
-          currentStep={this.state.currentStep} 
-          fileChange={this.fileChange}
-          file={this.state.file}
-          classes={this.classes}
-        />
-        {this.previousButton()}
-        {this.nextButton()}
+    nextButton(props) {
+        let currentStep = this.state.currentStep;
+        if (currentStep < 3) {
+            return (
+                <Button
+                    className={props.rightButton} color="secondary" variant="outlined"
+                    type="button" onClick={this._next}>
+                    Next
+                </Button>
+            )
+        }
+        return null;
+    }
 
-      </form>
-      </React.Fragment>
-    );
-  }
+    render() {
+        const { classes } = this.props;
+        return (
+            <ThemeProvider theme={theme}>
+                <React.Fragment>
+                    <h1>New Resource Wizard</h1>
+                    <p>Step {this.state.currentStep} </p>
+                    <form onSubmit={this.popUp}>
+
+                        <Step1
+                            currentStep={this.state.currentStep}
+                            handleChange={this.handleChange}
+                            name={this.state.formData.name}
+                            email={this.state.formData.email}
+                            title={this.state.formData.title}
+                            pri_contact_no={this.state.formData.pri_contact_no}
+                            sec_contact_no={this.state.formData.sec_contact_no}
+                            country={this.state.formData.country}
+                            region={this.state.formData.region}
+                            classes={classes}
+                        />
+                        <Step2
+                            currentStep={this.state.currentStep}
+                            handleChange={this.handleChange}
+                            comments={this.state.formData.comments}
+                            contract_type={this.state.formData.contract_type}
+                            management_co={this.state.formData.management_co}
+                        />
+                        <Step3
+                            currentStep={this.state.currentStep}
+                            fileChange={this.fileChange}
+                            fileName={this.state.fileName}
+                            classes={classes}
+                            handleSubmit={this.props.handleSubmit}
+                        />
+                        {this.previousButton(classes)}
+                        {this.nextButton(classes)}
+
+                    </form>
+                </React.Fragment>
+            </ThemeProvider>
+        );
+    }
 }
 
 function Step1(props) {
-  if (props.currentStep !== 1) {
-    return null
-  } 
+    if (props.currentStep !== 1) {
+        return null
+    }
+
+    const { classes } = props;
     return (
-        <div className="form-group">
-            <TextField fullWidth label="Full Name" name="name" value={props.name} onChange={props.handleChange} />
-            <TextField fullWidth label="Email" placeholder="user@kptv8.com" name="email" value={props.email} onChange={props.handleChange} />
-            <TextField fullWidth label="Title" variant="outlined" name="title" value={props.title} onChange={props.handleChange} />
-            <TextField fullWidth label="Primary Tel." variant="outlined" name="pri_contact_no" value={props.pri_contact_no} onChange={props.handleChange} />
-            <TextField fullWidth label="Secondary Tel." variant="outlined" name="sec_contact_no" value={props.sec_contact_no} onChange={props.handleChange} />
-            <TextField fullWidth label="Country" variant="outlined" name="country" value={props.country} onChange={props.handleChange} />
-            <TextField fullWidth label="Region" variant="outlined" name="region" value={props.region} onChange={props.handleChange} />
+        <div className="form-group" style={{ paddingTop: "1rem" }}>
+            <TextField label="Full Name" variant="outlined" name="name" value={props.name} onChange={props.handleChange} />
+            <TextField label="Email" variant="outlined" placeholder="user@kptv8.com" name="email" value={props.email} onChange={props.handleChange} />
+            <TextField label="Title" variant="outlined" name="title" value={props.title} onChange={props.handleChange} />
+            <TextField label="Primary Tel." variant="outlined" name="pri_contact_no" value={props.pri_contact_no} onChange={props.handleChange} />
+            <TextField label="Secondary Tel." variant="outlined" name="sec_contact_no" value={props.sec_contact_no} onChange={props.handleChange} />
+            <TextField label="Country" variant="outlined" name="country" value={props.country} onChange={props.handleChange} />
+            <TextField label="Region" variant="outlined" name="region" value={props.region} onChange={props.handleChange} />
         </div>
     );
 }
 
 function Step2(props) {
-  if (props.currentStep !== 2) {
-    return null
-  } 
-  return(
-    <div className="form-group">
-          <TextField fullWidth label="Contract Type (options)" variant="outlined" name="contract_type" value={props.contract_type} onChange={props.handleChange} />
-          <TextField fullWidth label="Management Co." variant="outlined" name="management_co" value={props.management_co} onChange={props.handleChange} />
-          <TextField fullWidth label="Comments" variant="outlined" name="comments" value={props.comments} onChange={props.handleChange} />
-
-    </div>
-  );
-}
-
-function Step3(props) {
-  if (props.currentStep !== 3) {
-    return null
-  } 
-
-  const hiddenFileInput = React.useRef(null);
-  const handleClick = event => {
-    hiddenFileInput.current.click();
-  };
+    if (props.currentStep !== 2) {
+        return null
+    }
     return (
-        <React.Fragment>
-            <div className="form-group">
-                
-                <Button className={styles.button} onClick={handleClick} variant="contained" color="primary">
-                    Upload
-                    <FileUpload />
-                </Button>
-                <input ref={hiddenFileInput} hidden name="file" type="file" onChange={props.fileChange} />
-            </div>
-            <button color="primary">Submit</button>
-        </React.Fragment>
+        <div className="form-group">
+            <TextField label="Contract Type (options)" variant="outlined" name="contract_type" value={props.contract_type} onChange={props.handleChange} />
+            <TextField label="Management Co." variant="outlined" name="management_co" value={props.management_co} onChange={props.handleChange} />
+            <TextField label="Comments" variant="outlined" name="comments" value={props.comments} onChange={props.handleChange} />
+
+
+        </div>
     );
 }
 
-export default MasterForm;
+function Step3(props) {
+    if (props.currentStep !== 3) {
+        return null
+    }
+    const { classes } = props;
+    const hiddenFileInput = React.useRef(null);
+    const handleClick = event => {
+        hiddenFileInput.current.click();
+    };
+
+    return (
+        <React.Fragment>
+            <div className="form-group">
+                <Button variant="contained" onClick={handleClick} color="default" className={classes.button} startIcon={<CloudUploadIcon />}>Upload</Button>
+                <label htmlFor="file">{props.fileName}</label>
+                <input ref={hiddenFileInput} hidden name="file" type="file" onChange={props.fileChange} />
+            </div>
+            <Button className={classes.rightButton} onClick={props.handleSubmit} variant="contained" color="primary">Submit</Button>
+        </React.Fragment>
+
+    );
+}
+
+export default withStyles(styles, { withTheme: true })(MasterForm);
