3744d76ba469966aa7265297dd8faedbde8e0e86
w8krl <44670608+w8krl@users.noreply.github.com>
2021-08-09 13:31:41 +0100

    Update NewResModal.js

--- app/containers/Modals/NewResModal.js
+++ app/containers/Modals/NewResModal.js
@@ -10,7 +10,7 @@ import NewRes from '../ResManagement/Forms/NewRes';
 import MultiStepFrm from '../ResManagement/Forms/AddResMultiStep';
 import { makeStyles } from '@material-ui/core/styles';
 
-// Test
+
 
 const useStyles = makeStyles((theme) => ({
     large: {
