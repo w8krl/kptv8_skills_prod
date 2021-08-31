84334dfbac72e11813db9ee925515626225fe093
w8krl <44670608+w8krl@users.noreply.github.com>
2021-08-31 06:22:37 +0100

    Update TimelineWidget.js

--- app/components/Widget/TimelineWidget.js
+++ app/components/Widget/TimelineWidget.js
@@ -10,43 +10,11 @@ import PapperBlock from '../PapperBlock/PapperBlock';
 import messages from './messages';
 import styles from './widget-jss';
 
-const dataTimeline = [
-  {
-    time: '11:20',
-    title: 'Updated Product',
-    desc: 'Quisque a consequat ante, at volutpat enim.'
-  },
-  {
-    time: 'Yesteray',
-    title: 'You liked James products',
-    desc: 'Aenean sit amet magna vel magna fringilla fermentum.'
-  },
-  {
-    time: 'Yesterday',
-    title: 'James just like your product',
-    desc: 'Nam posuere accumsan porta.'
-  },
-  {
-    time: '11 Oct 2018',
-    title: 'Jenna commented on your product',
-    desc: 'Curabitur egestas consequat lorem.'
-  },
-  {
-    time: 'Last week',
-    title: 'Jihan Doe just like your product',
-    desc: 'Vestibulum nec mi suscipit, dapibus purus a'
-  },
-  {
-    time: 'Last week',
-    title: 'James Doe just like your product',
-    desc: 'VCurabitur egestas consequat lorem.'
-  },
-];
 
 function TimelineWidget(props) {
   const { classes, intl, timelineData } = props;
-  return (
-    <PapperBlock whiteBg noMargin title={intl.formatMessage(messages.activity_title)} icon="av_timer" desc="">
+  return ( 
+    <PapperBlock whiteBg noMargin title={`${timelineData.length} ${intl.formatMessage(messages.activity_title)}`} icon="av_timer" desc="">
       <div className={classes.activityWrap}>
         <List>
           {timelineData.map((item, index) => (
